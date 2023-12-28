f4d471cd77b005cc8b11ed0bdb4952ccc5587f71
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-12-28 19:17:33 -0300

    chore: Format missing files

--- src/lib/components/Icon/Plus.svelte
+++ src/lib/components/Icon/Plus.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  export let size: string;
+	export let size: string;
 </script>
 
 <svg class="icon icon--plus" xmlns="http://www.w3.org/2000/svg" height={size} viewBox="0 0 512 512">
-  <!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) -->
-  <path
-    fill="currentColor"
-    d="M448 208H304V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H64c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
-  />
+	<!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) -->
+	<path
+		fill="currentColor"
+		d="M448 208H304V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H64c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
+	/>
 </svg>
